f0f7004a8cec82c566780ad66d54f92beff37269
Akshat Jain <jain.akshat31@gmail.com>
2020-08-26 21:57:11 +0530

    Minimum Cost for Tickets, Fizz Buzz

---
+++ August-Leetcode/day26.py
@@ -0,0 +1,23 @@
+'''
+Fizz Buzz
+
+Write a program that outputs the string representation of numbers from 1 to n.
+
+But for multiples of three it should output “Fizz” instead of the number and for the multiples of five output “Buzz”. For numbers which are multiples of both three and five output “FizzBuzz”.
+'''
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        res= []
+        for i in range(1,n+1):
+            if i%5==0 and i%3==0:
+                res.append("FizzBuzz")
+            elif i%3==0:
+                res.append("Fizz")
+            elif i%5==0:
+                res.append("Buzz")
+            else:
+                res.append(str(i))
+        return res
+
+#Runtime: 40 ms
+#Memory Usage: 15 MB
